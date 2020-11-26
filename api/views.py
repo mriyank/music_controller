ebd02bd25b3d50a3916a2b20f20385edd571c066
mriyank <mriyanksingh@gmail.com>
2020-11-26 19:22:15 +0530

    get room backend added

--- api/views.py
+++ api/views.py
@@ -14,6 +14,40 @@ class RoomView(generics.ListAPIView):
     serializer_class = RoomSerializer
 
 
+class GetRoom(APIView):
+    serializer_class = RoomSerializer
+    lookup_url_kwarg = 'code'
+
+    def get(self, request, format=None):
+        code = request.GET.get(self.lookup_url_kwarg)
+        if code != None:
+            room = Room.objects.filter(code=code)
+            if len(room) > 0:
+                data = RoomSerializer(room[0]).data
+                data['is_host'] = self.request.session.session_key == room[0].host
+                return Response(data, status=status.HTTP_200_OK)
+            return Response({'Room Not Found': 'Invalid Room Code.'}, status=status.HTTP_404_NOT_FOUND)
+
+        return Response({'Bad Request': 'Code paramater not found in request'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+# class GetRoom(APIView):
+#     serializer_class = RoomSerializer
+#     lookop_url_kwarg = 'code'
+
+#     def get(self, request, format=None):
+#         code = request.GET.get(self.lookop_url_kwarg)
+#         if code != None:
+#             room = Room.objects.filter(code=code)
+#             if len(room) > 0:
+#                 data = RoomSerializer(room[0]).data
+#                 data['is_host'] = self.request.session.session_key == room[0].host
+#                 return Response(data, status=status.HTTP_200_OK)
+#             return Response({'Room Not Found': 'Invalid room code'}, status=status.HTTP_404_NOT_FOUND)
+
+#         return Response({'Bad Request': 'Code parameter not found in request'}, status=status.HTTP_400_BAD_REQUEST)
+
+
 class CreateRoomView(APIView):
     serializer_class = CreateRoomSerializer
 
@@ -31,9 +65,9 @@ class CreateRoomView(APIView):
                 room = queryset[0]
                 room.guest_can_pause = guest_can_pause
                 room.votes_to_skip = votes_to_skip
-                room.save(update_fields=['guest_can_pause','votes_to_skip'])
+                room.save(update_fields=['guest_can_pause', 'votes_to_skip'])
                 return Response(RoomSerializer(room).data, status=status.HTTP_200_OK)
-            
+
             else:
                 room = Room(host=host, guest_can_pause=guest_can_pause,
                             votes_to_skip=votes_to_skip)
