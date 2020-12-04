77e4fd17fde63508f14c796d8a17939bb5e58550
mriyank <mriyanksingh@gmail.com>
2020-12-04 23:13:34 +0530

    room join page frontend

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import { render } from "react-dom";
 import HomePage from "./HomePage";
 
-
 export default class App extends Component {
   constructor(props) {
     super(props);
@@ -10,7 +9,7 @@ export default class App extends Component {
 
   render() {
     return (
-      <div>
+      <div className="center">
         <HomePage />
       </div>
     );
